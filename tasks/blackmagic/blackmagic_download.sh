4cdfede36e21d6f3b6d9813e6a0538cf33784895
Edmore Moyo <edmore.moyo@uct.ac.za>
2013-11-22 10:54:08 +0200

    OPENCAST-573 : Initial Ansible commit

---
+++ tasks/blackmagic/blackmagic_download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export http_proxy=http://arabica.cet.uct.ac.za:8999
+wget http://software.blackmagicdesign.com/DesktopVideo/Blackmagic_Desktop_Video_Linux_9.8.tar.gz
---
+++ tasks/lecturesight/opencl_download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export http_proxy=http://arabica.cet.uct.ac.za:8999
+wget http://registrationcenter.intel.com/irc_nas/3555/intel_sdk_for_ocl_applications_xe_2013_r2_sdk_3.1.1.11385_x64.tgz
