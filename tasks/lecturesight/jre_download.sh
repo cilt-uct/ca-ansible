d5527829035df5ba24438a9a27d9cd8fded594fd
Devan Govender <devan.govender@uct.ac.za>
2016-04-06 11:18:15 +0200

    lecturesight update and misc clean up

--- tasks/lecturesight/jre_download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget --no-proxy http://source.cet.uct.ac.za/opencastproject/galicaster/dependencies/jre-7u67-linux-x64.tar.gz
--- tasks/lecturesight/opencl_download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget --no-proxy http://source.cet.uct.ac.za/opencastproject/galicaster/dependencies/opencl.tgz
