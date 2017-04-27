fde9a36b0e7c1dbfa6b15130b9da62ebacc0d15d
Devan Govender <devan.govender@uct.ac.za>
2017-04-27 20:22:52 +0200

    OPENCAST-1223 Use the empty video analysis

--- templates/checkpresentations.py
+++ templates/checkpresentations.py
@@ -17,13 +17,17 @@ flavor_p1 = 'presentation/source'
 flavor_p2 = 'presentation2/source'
 
 # Threshold for considering a presentation track empty is 47.5kbps (Datapath "No signal" plus clock)
-bitrate_threshold = 47500
+bitrate_upper_threshold = 47500
+bitrate_lower_threshold = 15000
 
 # Bitrate threshold to compare contents for similar videos is 5%
 bitrate_diff_threshold = 0.05
 
 # Similarity threshold for dropping a video is 90%
 similarity_threshold = 90
+
+# Emptiness threshold for intermediate bitrate tracks
+empty_threshold = 90
 
 # Location of ffprobe and videomatch
 ffprobe_bin = 'ffprobe'
@@ -99,6 +103,7 @@ def drop_presentations(sender, operation_code, mp):
              except ValueError:
                 # ffprobe will return "N/A" for unknown bitrate (where the file was not closed properly)
                 logger.info('Unknown bitrate for track %s: %s', t.getURI(), ff_bitrate)
+                bitrate = -1
 
              if type == flavor_p1:
                 track_p1 = t
@@ -108,17 +113,33 @@ def drop_presentations(sender, operation_code, mp):
                 track_p2 = t
                 bitrate_p2 = bitrate
 
-    # Remove tracks with bitrate below threshold
+    # Remove tracks with bitrate below lower threshold
 
-    if (bitrate_p1 > 0) and (bitrate_p1 < bitrate_threshold):
+    if (bitrate_p1 > 0) and (bitrate_p1 < bitrate_lower_threshold):
        mp.remove(track_p1)
        removed = True
        logger.info('Presentation track ' + os.path.basename(track_p1.getURI()) + ' is probably empty and has been removed')
 
-    if (bitrate_p2 > 0) and (bitrate_p2 < bitrate_threshold):
+    if (bitrate_p2 > 0) and (bitrate_p2 < bitrate_lower_threshold):
        mp.remove(track_p2)
        removed = True
        logger.info('Presentation track ' + os.path.basename(track_p2.getURI()) + ' is probably empty and has been removed')
+
+    # If the bitrate is unknown or intermediate, check percentage empty
+
+    if (bitrate_p1 < 0) or ((bitrate_p1 >= bitrate_lower_threshold) and (bitrate_p1 < bitrate_upper_threshold)):
+       empty_p1 = __track_empty(track_p1)
+       if (empty_p1 > empty_threshold):
+          mp.remove(track_p1)
+          removed = True
+          logger.info('Presentation track ' + os.path.basename(track_p1.getURI()) + ' is %i%% empty and has been removed', empty_p1)
+
+    if (bitrate_p2 < 0) or ((bitrate_p2 >= bitrate_lower_threshold) and (bitrate_p2 < bitrate_upper_threshold)):
+       empty_p2 = __track_empty(track_p2)
+       if (empty_p2 > empty_threshold):
+          mp.remove(track_p2)
+          removed = True
+          logger.info('Presentation track ' + os.path.basename(track_p2.getURI()) + ' is %i%% empty and has been removed', empty_p2)
 
     # Check for duplicate tracks
 
@@ -154,4 +175,18 @@ def drop_presentations(sender, operation_code, mp):
        mp_repo.update(mp)
 
     logger.info("Finished")
+
+# Get the percentage blank score
+def __track_empty(t):
+
+    empty_result = subprocess.check_output([videomatch_bin, '--empty', t.getURI()]).replace("\n", "")
+    empty_result_i = 0
+
+    try:
+       empty_result_i = int(empty_result)
+       logger.info('Presentation track %s is %i%% empty', os.path.basename(t.getURI()), empty_result_i)
+    except ValueError:
+       logger.info('Unknown emptiness result for track %s: %s', empty_result)
+
+    return empty_result_i;
 
