8a2812268b15e326966875b94d70cac40df5e7ff
Devan Govender <devan.govender@uct.ac.za>
2018-07-26 09:37:22 +0200

    OPENCAST-1605 Wake up next day at 6am

--- templates/wakeup.sh
+++ templates/wakeup.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 echo 0 > /sys/class/rtc/rtc0/wakealarm
-echo `date '+%s' -d '+ 24 hours'` > /sys/class/rtc/rtc0/wakealarm
+echo `date '+%s' -d 'tomorrow 06:00'` > /sys/class/rtc/rtc0/wakealarm
 cat /sys/class/rtc/rtc0/wakealarm > /var/log/last_known_wakeup_time
 
